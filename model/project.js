093e65bb3c90c6a0dd467b902067bfe6bce5323b
Erick Gledson <ericksilva@outlook.com>
2020-10-11 11:22:42 -0300

    added project attributes

--- model/project.js
+++ model/project.js
@@ -6,10 +6,89 @@ const projectSchema = new Schema({
         type: String,
         required: true,
     },
-    name: {
+    social_name: {
         type: String,
         required: true,
         maxlength: 150,
+    },
+    fantasy_name: {
+        type: String,
+        required: true,
+        maxlength: 150,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    cnae: {
+        type: String,
+        required: true,
+    },
+    cnpj: {
+        type: String,
+        required: true,
+    },
+    open_date: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        maxlength: 11,
+    },
+    cep: {
+        type: String,
+        required: true,
+        maxlength: 9
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    complement: {
+        type: String,
+        required: false,
+    },
+    neighborhood: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    segment: {
+        type: String,
+        required: true,
+    },
+    community_impact: {
+        type: String,
+        required: true,
+    },
+    credit_reason: {
+        type: String,
+        required: true,
+    },
+    total_amount: {
+        type: String,
+        required: true,
+    },
+    collected_amount: {
+        type: String,
+        required: true,
     },
 });
 
